5bca876188e20638e2a8ba34416ebcfecc6d9821
MaximOdincov <124523356+MaximOdincov@users.noreply.github.com>
2024-11-03 13:26:00 +0500

    i

--- src/Being a merchant/Assets/Scripts/Clickable Obj/Point.cs
+++ src/Being a merchant/Assets/Scripts/Clickable Obj/Point.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 using Unity.VisualScripting;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class Point : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public float animationDuration;
+    public TextMeshProUGUI text;
     public float size;
+    public void Start()
+    {
+        if (text != null)
+        {
+            text.enabled = false;
+        }
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (text != null)
+        {
+            text.enabled = true;
+        }
         StartCoroutine(Animate(size));
         Debug.Log("Навелись!");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (text != null)
+        {
+            text.enabled = false;
+        }
         StartCoroutine(Animate(-size));
         Debug.Log("Не навелись");
     }
