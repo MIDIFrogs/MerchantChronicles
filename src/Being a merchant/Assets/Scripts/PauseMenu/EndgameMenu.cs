47b6bb9639ca29b10e47bd07459cf21d9c3db143
Ivan Olyanishin <ivan.ol.1704@gmail.com>
2024-11-04 20:05:06 +0500

    IdkWhatI've donw

---
+++ src/Being a merchant/Assets/Scripts/PauseMenu/EndgameMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SibGameJam
+{
+	public class EndgameMenu : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI rep;
+		[SerializeField] private TextMeshProUGUI coins;
+
+		[SerializeField] private GameObject win;
+		[SerializeField] private GameObject lose;
+
+		[SerializeField] private AudioSource aourse;
+		[SerializeField] private AudioClip goodSound;
+		[SerializeField] private AudioClip badSound;
+
+		public void Start()
+		{
+			rep.text = PlayerStats.Session.Reputation.ToString();
+			coins.text = PlayerStats.Session.Coins.ToString();
+			if (PlayerStats.Session.Coins > 100 && PlayerStats.Session.Reputation > 0)
+			{
+				win.SetActive(true);
+				aourse.clip = goodSound;
+			}
+			else
+			{
+				lose.SetActive(true);
+				aourse.clip = badSound;
+			}
+			aourse.Play();
+			GameSession.DeleteSession();
+			StartCoroutine(BackToMenu());
+		}
+
+		public IEnumerator BackToMenu()
+		{
+			yield return new WaitForSeconds(10);
+			SceneManager.LoadScene("Menu");
+		}
+	}
+}
